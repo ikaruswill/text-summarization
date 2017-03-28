130cd0a284f6e1db4420cc762b4babc55266f3c6
Will Ho <ikaruswill.ho@gmail.com>
2017-03-29 01:06:38 +0800

    Implement initial inputdocument.py

--- parser.py
+++ parser.py
@@ -3,34 +3,6 @@ from preprocess import remove_stopwords
 
 class Parser(object):
 
-	def __init__(self, text, path):
-		full_annotator_list = ["tokenize", "ssplit", "pos", "lemma", "ner", "parse", "dcoref"]
-		cn = pywrap.CoreNLP(url='http://localhost:9000', annotator_list=full_annotator_list)
-		self.text = text
-		result = cn.basic(text, out_format='json')
-		extract_named_entities(result)
-		extract_coreferences(result)
-		build_word_to_lemma_dict(result)
-		prepare_paragraphs(result)
-
-	def extract_named_entities(self, result):
-		self.named_entities = []
-		for sentence in result:
-			for token in sentence['tokens']:
-				if token['ner'] != 'O':
-					self.named_entities.append(token['originalText'])
-
-	def extract_coreferences(self, result):
-		pass
-
-	def build_word_to_lemma_dict(self, result):
-		tokens = [token for sentence in result for token in sentence['tokens']]
-		tokens = remove_stopwords(tokens, 'originalText')
-
-		self.word_to_lemma_dict = {}
-		for token in tokens:
-			self.word_to_lemma_dict[token['orignalText']] = token['lemma']
-		
-	def prepare_paragraphs(self, result):
+	def __init__(self):
 		pass
 
