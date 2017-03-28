2b67040749910a84fbe5e0752797a44328c0727d
Will Ho <ikaruswill.ho@gmail.com>
2017-03-28 22:49:03 +0800

    Update stopword removal function to be key aware

--- preprocess.py
+++ preprocess.py
@@ -3,5 +3,5 @@ from nltk.corpus import stopwords
 stopwords = set(stopwords.words('english'))
 
 # Tokens must be stripped
-def remove_stopwords(tokens):
-	return [token for token in tokens if token not in stopwords]
+def remove_stopwords(tokens, key):
+	return [token for token in tokens if token[key] not in stopwords]
