11f4fe913b541ada14ea05ba60d33ff1a05058f3
Will Ho <ikaruswill.ho@gmail.com>
2017-03-29 17:02:41 +0800

    Combined string_utils into utility.py

--- utility.py
+++ utility.py
@@ -1,4 +1,23 @@
+from nltk.util import ngrams
+
 def load_file(file_path):
 	with open(file_path, 'r') as f:
 		text = f.read()
 	return text
+
+def count_words(text):
+	return len(text.split())
+
+def generate_unigrams(text):
+	return list(ngrams(text.lower(), 1))
+
+def generate_bigrams(text):
+	return list(ngrams(text.lower(), 2))
+
+def split_string_to_words(text):
+	return text.split()
+
+def increment_value(self, dictionary, key):
+	if key not in dictionary:
+		dictionary[key] = 0
+	dictionary[key] += 1
