347f0f167a1f42956c60a55bd9d7067a8b940ef3
aparna-bansal <aparna.bansal.03@gmail.com>
2017-04-01 17:28:56 +0800

    To be tested.

---
+++ PhraseScorer.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import math
+from sets import Set
+from units import Paragraph
+from units import Phrase
+import InputDocument
+
+class PhraseScorer():
+    
+    B = 6.0
+    RHO = 0.5
+    
+    def _init_(self, input_doc):
+        self.input_document = input_doc
+        
+    def weighting_paragraph(self, paragraph_position):
+        if paragraph_position < -1 * math.log(B)/math.log(RHO):
+            return math.pow(RHO, paragraph_position)*B
+        else:
+            return 1.0
+        
+    def score_phrase(self, phrase):
+        score = 0.0
+        concept = phrase.get_concepts()
+        paragraphs = input_document.paragraphs
+        paragraphLength = len(paragraphs)
+        for concept in concepts:{
+                for i in range(paragraghLength):{
+                    count = paragraphs[i].count_frequency(concept)
+                    score += count * weighting_paragraph(i)
+                }
+            }
+            return score          
+
