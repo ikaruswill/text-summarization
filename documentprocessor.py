8da06a1a1c39fcef29c6f4493691a39d18b2f744
Will Ho <ikaruswill.ho@gmail.com>
2017-04-02 01:27:25 +0800

    Implement documentprocessor with process_document and extract_phrases

---
+++ documentprocessor.py
@@ -0,0 +1,33 @@
+from phraseextractor import PhraseExtractor
+
+class DocumentProcessor():
+	def __init__(self, is_tac, indicator_matrix):
+		self.indicator_matrix = indicator_matrix
+		self.is_tac = is_tac
+		self.docs = []
+		self.noun_phrases = []
+		self.verb_phrases = []
+		self.all_phrases = []
+
+		self.nouns = []
+		self.verbs = []
+		self.coreferences = {}
+
+	def process_document(self, text):
+		doc = InputDocument(text)
+		self.docs.append(doc)
+		self.extract_phrases(doc)
+		self.coreferences.update(doc.coreferences)
+
+	def extract_phrases(self, doc):
+		phrase_extractor = PhraseExtractor(doc, self.indicator_matrix)
+		phrases = phrase_extractor.extract_all_phrases()
+
+		for phrase in phrases:
+			if phrase.is_NP:
+				self.noun_phrases.append(phrase)
+				self.nouns.append(phrase.content)
+			else:
+				self.verb_phrases.append(phrase)
+				self.verbs.append(phrase.content)
+			self.all_phrases.append(phrase)
