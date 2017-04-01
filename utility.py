e0ea02b597035021b19bc6564b23d401b246b37d
Will Ho <ikaruswill.ho@gmail.com>
2017-04-01 16:03:02 +0800

    Fix stopwords reference error

--- utility.py
+++ utility.py
@@ -17,8 +17,8 @@ def generate_bigrams(tokens):
 	return [' '.join(bigram) for bigram in ngrams(tokens, 2)]
 
 def remove_stopwords(tokens, key):
-	stopwords = set(stopwords.words('english'))
-	return [token for token in tokens if token[key] not in stopwords]
+	stopwords_set = set(stopwords.words('english'))
+	return [token for token in tokens if token[key] not in stopwords_set]
 
 def increment_value(dictionary, key):
 	if key not in dictionary:
