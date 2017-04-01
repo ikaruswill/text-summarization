ce875eb85dd63497423bcde4e73c696a8b90443c
Will Ho <ikaruswill.ho@gmail.com>
2017-04-02 01:48:34 +0800

    Implement calculate_jaccard_index in parser

--- parser.py
+++ parser.py
@@ -3,3 +3,18 @@ class Parser():
 	def __init__(self):
 		pass
 
+	def calculate_jaccard_index(phrase1, phrase2):
+		concepts_phrase1 = phrase1.concepts
+		concepts_phrase2 = phrase2.concepts
+
+		count = 0
+		for concept in concepts_phrase1:
+			if concepts_phrase2.find(concept):
+				count += 1
+
+		divisor = len(concepts_phrase1) + len(concepts_phrase2) - count
+		
+		if divisor == 0:
+			return 0.0
+		else:
+			return float(count) / divisor
