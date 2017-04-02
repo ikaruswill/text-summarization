e95877a431cf6e139e188a2d6b64a9f838c2172d
Will Ho <ikaruswill.ho@gmail.com>
2017-04-02 22:42:46 +0800

    Move InputDocument into units

--- parser.py
+++ parser.py
@@ -4,16 +4,18 @@ import gurobipy as g
 import sys
 import argparse
 import os
+import utility
 
-DEFAULT_MAXIMUM_SENTENCE = 10;
-DEFAULT_ALTERNATIVE_VP_THRESHOLD = 0.75;
-DEFAULT_MAX_WORD_LENGTH = 100;
+DEFAULT_MAXIMUM_SENTENCE = 10
+DEFAULT_ALTERNATIVE_VP_THRESHOLD = 0.75
+DEFAULT_MAX_WORD_LENGTH = 100
+DEFAULT_THREADS = 0
 
 class Parser():
-	MIN_SENTENCE_LENGTH = 5;
-	MINIMUM_VERB_LENGTH = 2;
+	MIN_SENTENCE_LENGTH = 5
+	MINIMUM_VERB_LENGTH = 2
 
-	def __init__(self, max_sentence, alternative_vp_threshold, max_word_length, is_TAC, threads=1):
+	def __init__(self, max_sentence, alternative_vp_threshold, max_word_length, is_tac, threads):
 		self.max_sentence = max_sentence
 		self.alternative_vp_threshold = alternative_vp_threshold
 		self.max_word_length = max_word_length
@@ -40,7 +42,7 @@ class Parser():
 		self.noun_to_noun_variables = {}
 		self.verb_to_verb_variables = {}
 
-		self.processor = DocumentProcessor()
+		self.processor = DocumentProcessor(is_tac, self.indicator_matrix)
 
 	def build_compatibility_matrix(self):
 		for np in self.noun_phrases:
@@ -383,16 +385,19 @@ class Parser():
 		self.score_phrases()
 		self.find_optimal_solution()
 
+	def process_document(self, string):
+		self.processor.process_document(string)
+
 
 def main():
-	parser = Parser()
+	parser = Parser(args.max_sent, args.alt_vp_thresh, args.max_word_length, not args.plaintext, args.threads)
 	for dirpath, dirnames, filenames in os.walk(args.input_dir):
 		for filename in filenames:
 			if filename.startswith('.'):
 				continue
 			file_path = os.path.join(dirpath, filename)
 			text = utility.load_file(file_path)
-			parser.processDocument(text)
+			parser.process_document(text)
 
 		parser.update_model()
 		summary = parser.generate_summary()
@@ -403,11 +408,11 @@ def main():
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Abstractive Summarizer')
 	parser.add_argument('input_dir', help='input folder containing all text files')
-	parser.add_argument('out_file', help='output file')
-	parser.add_argument('-w', '--word-length', help='maximum word length', default=DEFAULT_MAX_WORD_LENGTH)
-	parser.add_argument('-v', '--vp-thresh', help='alternative VP threshold', default=DEFAULT_ALTERNATIVE_VP_THRESHOLD)
+	parser.add_argument('output_file', help='output file')
+	parser.add_argument('-w', '--max-word-length', help='maximum word length', default=DEFAULT_MAX_WORD_LENGTH)
+	parser.add_argument('-v', '--alt-vp-thresh', help='alternative VP threshold', default=DEFAULT_ALTERNATIVE_VP_THRESHOLD)
 	parser.add_argument('-s', '--max-sent', help='maximum number of sentences', default=DEFAULT_MAXIMUM_SENTENCE)
-	parser.add_argument('-t', '--threads', help='number of threads', default=0)
+	parser.add_argument('-t', '--threads', help='number of threads', default=DEFAULT_THREADS)
 	parser.add_argument('-p', '--plaintext', help='is plain text data', default=False, action='store_true')
 	parser.add_argument('-e', '--export-only', help='only export the phrases', default=False, action='store_true')
 	args = parser.parse_args()
--- units.py
+++ units.py
@@ -1,4 +1,7 @@
+from corenlp_pywrap import pywrap
+import xml.etree.ElementTree
 import utility
+import re
 
 class Phrase():
 	content = None
@@ -119,3 +122,113 @@ class PhraseMatrix(dict):
 		key += str(phrase_tuple[1].phrase_id)
 
 		super().__setitem__(key, value)
+
+
+class InputDocument():
+	__p_marker = '\n\n'
+
+	def __init__(self, input_str, is_tac=True):
+		full_annotator_list = ["tokenize", "ssplit", "pos", "lemma", "ner", "parse", "dcoref"]
+		cn = pywrap.CoreNLP(url='http://localhost:9000', annotator_list=full_annotator_list)
+		
+		if is_tac:
+			text = self.parse_xml_string(input_str)
+		else:
+			text = input_str
+
+		# Note: corenlp recognizes sentences delimiters as periods regardless of newlines
+		result = cn.basic(text, out_format='json').json()
+		# CAUTION: sentences may not coincide with the number of sentences in parse_trees
+		self.sentences = utility.sent_tokenize(text)
+		self.parse_trees = [sentence['parse'] for sentence in result['sentences']]
+		self.named_entities = self.extract_named_entities(result)
+		self.coreferences = self.extract_coreferences(result)
+		self.word_to_lemma_dict = self.build_word_to_lemma_dict(result)
+		self.paragraphs = self.prepare_paragraphs(text, result)
+
+	def parse_xml_string(self, xml_string):
+		root = xml.etree.ElementTree.fromstring(xml_string)
+		self.headline = root.find('.//HEADLINE').text
+		text = ''
+		# To replace \n in line breaks within <P> tag
+		# Note: Minor issue with with 16\n-year-old --> 16 -year-old
+		newline_regex = re.compile('\n')
+		for child in root.find('.//TEXT'):
+			if child.tag == 'P':
+				text += newline_regex.sub(' ', child.text.strip()) + self.__class__.__p_marker
+		return text.strip()
+
+	def extract_named_entities(self, result):
+		named_entities = []
+		for sentence in result['sentences']:
+			for token in sentence['tokens']:
+				if token['ner'] != 'O':
+					named_entities.append(token['originalText'])
+		return named_entities
+
+	def extract_coreferences(self, result):
+		corefs = {}
+
+		for coref_chain in result['corefs'].values():
+			if len(coref_chain) == 1:
+				continue
+
+			mentions_set = set()
+			for mention in coref_chain:
+				mentions_set.add(mention['text'])
+				if mention['isRepresentativeMention']:
+					representative_mention = mention
+
+			corefs[representative_mention['text']] = mentions_set
+
+		return corefs
+
+	def build_word_to_lemma_dict(self, result):
+		tokens = [token for sentence in result['sentences'] for token in sentence['tokens']]
+		tokens = utility.remove_stopwords(tokens, 'originalText')
+
+		word_to_lemma_dict = {}
+		for token in tokens:
+			word_to_lemma_dict[token['originalText']] = token['lemma']
+
+		return word_to_lemma_dict
+		
+	def prepare_paragraphs(self, text, result):
+		paragraphs = []
+		paragraphs_text = text.split(self.__class__.__p_marker)
+		for paragraph_text in paragraphs_text:
+			paragraph_concept_frequency = self.extract_concepts_from_string(paragraph_text)
+			paragraph = Paragraph(paragraph_concept_frequency)
+			paragraphs.append(paragraph)
+		
+		return paragraphs
+
+	def get_sentences(self, text):
+		return utility.sent_tokenize(text)
+
+	def extract_concepts_from_string(self, string):
+		concept_frequency_dict = {}
+
+		# Consider using Counter class as optimization
+		tokens = utility.word_tokenize(string)
+		encountered = set()
+		lemma_unigrams = []
+		for token in tokens:
+			if token in self.word_to_lemma_dict:
+				lemma = self.word_to_lemma_dict[token]
+				if lemma not in encountered:
+					encountered.add(lemma)
+					lemma_unigrams.append(lemma)
+				utility.increment_value(concept_frequency_dict, lemma)
+
+		# Note: Bigrams are generated from ordered unique lemmas not original string
+		lemma_bigrams = utility.generate_bigrams(lemma_unigrams)
+		for bigram in lemma_bigrams:
+			utility.increment_value(concept_frequency_dict, bigram)
+
+		# Note: Named entities may duplicate with unigrams or bigrams as they are in un-preprocessed form
+		for entity in self.named_entities:
+			if entity in string:
+				utility.increment_value(concept_frequency_dict, entity)
+
+		return concept_frequency_dict
