4096ae21d827b06fe44f9d715b79c82e603939be
Will Ho <ikaruswill.ho@gmail.com>
2017-04-03 11:12:46 +0800

    Refactor calculate_jaccard_index into utility

--- parser.py
+++ parser.py
@@ -63,22 +63,6 @@ class Parser():
 
 		return self.calculate_jaccard_index(phrase1, phrase2)
 
-	def calculate_jaccard_index(self, phrase1, phrase2):
-		concepts_phrase1 = phrase1.concepts
-		concepts_phrase2 = phrase2.concepts
-
-		count = 0
-		for concept in concepts_phrase1:
-			if concepts_phrase2.find(concept):
-				count += 1
-
-		divisor = len(concepts_phrase1) + len(concepts_phrase2) - count
-		
-		if divisor == 0:
-			return 0.0
-		else:
-			return float(count) / divisor
-
 	def score_phrases(self):
 		for doc in self.docs:
 			scorer = PhraseScorer(doc)
--- utility.py
+++ utility.py
@@ -30,3 +30,19 @@ def increment_value(dictionary, key):
 
 def build_key(phrase1, phrase2):
 		return phrase1.phrase_id + ':' + phrase2.phrase_id
+
+def calculate_jaccard_index(self, phrase1, phrase2):
+	concepts_phrase1 = phrase1.concepts
+	concepts_phrase2 = phrase2.concepts
+
+	count = 0
+	for concept in concepts_phrase1:
+		if concepts_phrase2.find(concept):
+			count += 1
+
+	divisor = len(concepts_phrase1) + len(concepts_phrase2) - count
+	
+	if divisor == 0:
+		return 0.0
+	else:
+		return float(count) / divisor
